66aa1f99b17aa845ceeae247fa2dd5a54200fc7f
cshive5 <cshive5@yahoo.com>
2015-03-13 17:15:56 -0400

    initial commit

---
+++ app/models/organization.rb
@@ -0,0 +1,13 @@
+# == Schema Information
+#
+# Table name: organizations
+#
+#  id         :integer          not null, primary key
+#  name       :string(255)
+#  identifier :string(255)
+#  created_at :datetime
+#  updated_at :datetime
+#
+
+class Organization < ActiveRecord::Base
+end
---
+++ app/views/organizations/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @organization, :id, :name, :identifier, :created_at, :updated_at
