0fcedac9913a57ed2ec0f751ae1d23c64a52ff88
cshive <Welcome0!>
2015-04-02 17:11:09 -0400

    fixed controller

--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -5,7 +5,7 @@ class OrganizationsController < ApplicationController
   # GET /organizations
   # GET /organizations.json
   def index
-    @organizations = Organization.alls
+    @organizations = Organization.all
   end
 
   # GET /organizations/1
