a7e062d812cce87bcaffb55f299fdc08b85b6a5b
Wu <wus4@wvpn-156-40-193-107.net.nih.gov>
2015-04-02 15:52:47 -0400

    Bootstrap a test user

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,3 +8,10 @@
 
 Organization.create(name: 'MD Anderson', identifier: 'tx035')
 Organization.create(name: 'GBMC', identifier: 'md036')
+
+user = User.new(email: 'test@test.com',
+                password: 'Welcome01',
+                password_confirmation: 'Welcome01'
+               )
+user.skip_confirmation!
+user.save!
