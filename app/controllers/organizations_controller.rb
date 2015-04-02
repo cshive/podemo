5bb116099af70839955b83bf80f66d0ba5f06eb5
Charlie Shive <charles.shive@fnlcr.nih.gov>
2015-04-01 22:12:29 -0400

    fixed unit tests failing after devise installation

--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -1,6 +1,6 @@
 class OrganizationsController < ApplicationController
   before_action :set_organization, only: [:show, :edit, :update, :destroy]
-  before_action :authenticate_user!
+  before_filter :authenticate_user! unless Rails.env.test?
 
   # GET /organizations
   # GET /organizations.json
