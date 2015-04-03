7519c3b1de424ee96439a29f65e114d87c449fd9
Wu <wus4@wvpn-156-40-193-48.net.nih.gov>
2015-04-03 13:10:00 -0400

    Fixed CSRF token can't be verified issue for AngularJS

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,23 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   #protect_from_forgery with: :exception
   protect_from_forgery with: :null_session
+
+  # CSRF protection for AngularJS
+  after_filter :set_csrf_cookie_for_ng
+
+  def set_csrf_cookie_for_ng
+    cookies['XSRF-TOKEN'] = form_authenticity_token if protect_against_forgery?
+  end
+
+  protected
+
+  def verified_request?
+    if respond_to?(:valid_authenticity_token?, true)
+      # Rails 4.2 and above
+      super || valid_authenticity_token?(session, request.headers['X-XSRF-TOKEN'])
+    else
+      # Rails 4.1 and below
+      super || form_authenticity_token == request.headers['X-XSRF-TOKEN']
+    end
+  end
 end
