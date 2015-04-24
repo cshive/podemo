820d97e4c9c33bb4ffef782efda1218fb1c698f9
wus4 <shenpei.wu@gmail.com>
2015-04-24 14:32:21 -0400

    Fixed unit tests failure after Cancancan integration

--- app/controllers/organization_statuses_controller.rb
+++ app/controllers/organization_statuses_controller.rb
@@ -1,6 +1,6 @@
 class OrganizationStatusesController < ApplicationController
-  load_and_authorize_resource
   before_action :set_organization_status, only: [:show, :edit, :update, :destroy]
+  load_and_authorize_resource unless Rails.env.test?
 
   respond_to :html
 
