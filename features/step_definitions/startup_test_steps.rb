057a38e502d5476acef9191049720db377ff618c
my2239 <ymahesh@gmail.com>
2015-04-24 08:04:01 -0400

    test commit

--- features/step_definitions/startup_test_steps.rb
+++ features/step_definitions/startup_test_steps.rb
@@ -4,9 +4,9 @@ require 'selenium-webdriver'
 
 driver = Selenium::WebDriver.for :chrome
 driver.get "http://localhost:3000"
-driver = Selenium::WebDriver.for :firefox
-driver.get "http://localhost:3000"
-driver = Selenium::WebDriver.for :safari
-driver.get "http://localhost:3000"
+# driver = Selenium::WebDriver.for :firefox
+# driver.get "http://localhost:3000"
+# driver = Selenium::WebDriver.for :safari
+# driver.get "http://localhost:3000"
 
 end
