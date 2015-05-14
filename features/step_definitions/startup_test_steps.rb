3ab96b0069f5a77dc90207fc8a51cc6bd19911a0
shilpi85 <shilpi.rauniyar@gmail.com>
2015-05-14 09:41:43 -0400

    use Safari browser instead of Chrome

--- features/step_definitions/startup_test_steps.rb
+++ features/step_definitions/startup_test_steps.rb
@@ -2,8 +2,8 @@ Given /^User navigates to provided url$/ do
 require 'rubygems'
 require 'selenium-webdriver'
 
-driver = Selenium::WebDriver.for :chrome
-driver.get "http://localhost"
+driver = Selenium::WebDriver.for :safari
+driver.get "http://localhost:3000"
 
 end
 
