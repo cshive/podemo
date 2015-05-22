3b7d260bc27146716bdc1a89b8151b043ea244e3
cshive <Welcome0!>
2015-05-22 14:32:48 -0400

    fixed angular public folder structure

--- config/routes.rb
+++ config/routes.rb
@@ -9,8 +9,6 @@ Rails.application.routes.draw do
 
     mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
 
-    get '/angular', :to => redirect('/podemo/angular/')
-
     resources :organizations do
       collection do
         post 'create_from_rest_api'
