c1f8f4fc5481cf16e551f9d312104c1391540505
wus4 <shenpei.wu@gmail.com>
2015-07-08 15:28:23 -0400

    change to chrome

--- Conf.js
+++ Conf.js
@@ -6,7 +6,7 @@ exports.config = {
     seleniumAddress: 'http://localhost:4444/wd/hub',
 
     capabilities: {
-        'browserName' : ''
+        'browserName' : 'chrome'
     },
 
  //   specs: ['test_spec1.js'],
