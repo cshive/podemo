9d4218110c1331b2458f50117f5b8a7f2a437249
shilpi85 <shilpi.rauniyar@gmail.com>
2015-07-08 11:50:02 -0400

    changing browser to firefox

--- Conf.js
+++ Conf.js
@@ -6,7 +6,7 @@ exports.config = {
     seleniumAddress: 'http://localhost:4444/wd/hub',
 
     capabilities: {
-        'browserName' : 'chrome'
+        'browserName' : 'firefox'
     },
 
  //   specs: ['test_spec1.js'],
