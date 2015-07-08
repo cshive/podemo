fb3d98982a377ee312e22068b0ca77963283d719
wus4 <shenpei.wu@gmail.com>
2015-07-08 15:25:38 -0400

    no browser

--- Conf.js
+++ Conf.js
@@ -6,7 +6,7 @@ exports.config = {
     seleniumAddress: 'http://localhost:4444/wd/hub',
 
     capabilities: {
-        'browserName' : 'google-chrome'
+        'browserName' : ''
     },
 
  //   specs: ['test_spec1.js'],
