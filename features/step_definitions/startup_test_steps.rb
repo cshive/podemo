c23fb6807a86195777ea8d9f4d1365f5c13a2cd6
shilpi85 <shilpi.rauniyar@gmail.com>
2015-07-09 09:41:58 -0400

    test firefox

--- features/step_definitions/startup_test_steps.rb
+++ features/step_definitions/startup_test_steps.rb
@@ -1,14 +1,18 @@
 #require 'rubygems'
 #require 'selenium-webdriver'
+driver1 = Selenium::WebDriver.for :firefox
 
 Given "User navigates to provided url11" do
 
-driver1 = Selenium::WebDriver.for :firefox
-driver1.get "http://localhost/podemo/angular/#/main/welcome"
+
+driver1.get "http://localhost/podemo/"
 
 end
 
 Then(/^he should see two links1$/) do
+  driver1.find_element(:xpath, "//div[@id='inner']/div/a/img").click
+  driver1.find_element(:link, "New Organization").click
+  driver1.quit
   puts("This is a test that is purposely designed to pass")
 end
 # Test comment
