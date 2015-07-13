b94d568c2124afc0bccdf3c65b21dddf70da9c4d
wus4 <shenpei.wu@gmail.com>
2015-07-13 15:17:42 -0400

    Added seleniumAddress back

--- Conf.js
+++ Conf.js
@@ -3,7 +3,7 @@
  */
 
 exports.config = {
-    //seleniumAddress: 'http://localhost:4444/wd/hub',
+    seleniumAddress: 'http://localhost:4444/wd/hub',
 
     capabilities: {
         'browserName' : 'firefox'
